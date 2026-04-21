d31fb59fc381115c2de5b2be5d479d4c8791ab26
Friptu Teodor <friptut@gmail.com>
2026-04-21 15:05:01 +0300

    adding unit tests for GetAll and GetById product handlers

---
+++ simplifiedEcom/tests/Ecommerce.Tests/Features/Products/GetAll/GetAllProductsTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Ecommerce.Tests.Features.Products.GetAll;
+
+using Api.Domain.Product;
+using Api.Features.Products.GetAll;
+using Api.Infrastructure.Repositories.ProductRepository;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Moq;
+
+public class GetAllProductsTests
+{
+    private readonly Mock<IProductRepository> _repositoryMock;
+    private readonly GetAllProductsHandler _handler;
+
+    public GetAllProductsTests()
+    {
+        _repositoryMock = new Mock<IProductRepository>();
+        _handler = new GetAllProductsHandler(_repositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_Should_Return_200Ok_With_Products()
+    {
+        // Arrange 
+        var products = new List<GetAllProductDto>
+        {
+            new GetAllProductDto
+            {
+                Id = 1,
+                ImageUrl = "images/laptop.jpg",
+                Name = "MacBook Air",
+                Price = 2000
+            },
+            new GetAllProductDto
+            {
+                Id = 2,
+                ImageUrl = "images/phone.jpg",
+                Name = "MacBook Air2",
+                Price = 2500
+            }
+        };
+        _repositoryMock
+            .Setup(repo => repo.GetAllAsync())
+            .ReturnsAsync(products);
+
+        // Act
+        var result = await _handler.Handle();
+
+        // Assert
+        result.Should().BeOfType<Ok<List<GetAllProductDto>>>();
+    }
+}
---
+++ simplifiedEcom/tests/Ecommerce.Tests/Features/Products/GetById/GetByIdProductTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Ecommerce.Tests.Features.Products.GetById;
+
+using Api.Domain.Product;
+using Api.Features.Products.GetAll;
+using Api.Features.Products.GetById;
+using Api.Infrastructure.Repositories.ProductRepository;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Moq;
+
+public class GetByIdProductTests
+{
+    private readonly Mock<IProductRepository> _repo;
+    private readonly GetByIdProductHandler _handler;
+
+    public GetByIdProductTests()
+    {
+        _repo = new Mock<IProductRepository>();
+        _handler = new GetByIdProductHandler(_repo.Object);
+    }
+
+    [Fact]
+    public async Task Handle_Should_Return_200Ok_With_One_Product()
+    {
+        // Arrange 
+        var product = new Product
+        {
+            Id = 3,
+            ImageUrl = "/images/Laptop.jpg",
+            Name = "G Class",
+            Price = 300000
+        };
+        _repo.Setup(repo => repo.GetByIdAsync(product.Id))
+            .ReturnsAsync(product);
+        // Act
+        var result = await _handler.Handle(product.Id);
+
+        // Assert
+        result.Should().BeOfType<Ok<Product>>();
+    }
+}
